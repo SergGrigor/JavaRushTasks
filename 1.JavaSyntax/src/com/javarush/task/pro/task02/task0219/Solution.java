9b1235c2adbfb489c5ca2a8bcf1d8d12296af72d
SergGrigor <vikingmasterserg@gmail.com>
2022-04-21 00:19:38 +0300

    Add solution JavaSyntax task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int id = 1;
+        int age = 35;
+        String documents = "Passport";
+        String behavior = "Good";
+    }
+}

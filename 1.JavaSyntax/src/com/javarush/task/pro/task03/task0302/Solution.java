f9c6277b3aba417cea7d87a2c8651f59a2d1a977
SergGrigor <vikingmasterserg@gmail.com>
2022-04-21 23:11:19 +0300

    Add solution JavaSyntax task 0302

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+/* 
+Призывная кампания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+        //напишите тут ваш код
+        Scanner key = new Scanner(System.in);
+        String name = key.nextLine();
+        int age = key.nextInt();
+
+        //выполнение условия, возраст 18-28
+        if (age >= 18 & age <= 28)
+            System.out.println(name + militaryCommissar);
+    }
+}

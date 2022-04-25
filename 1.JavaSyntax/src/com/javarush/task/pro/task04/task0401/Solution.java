5c33bbe6e633f7dd5cd4d4e656fe8ee1ab6e34a2
SergGrigor <vikingmasterserg@gmail.com>
2022-04-25 12:58:19 +0300

    Add solution JavaSyntax task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        //напишите тут ваш код
+        int circle = 0;
+        while (circle < 100) {
+            System.out.println(quote);
+            circle++;
+        }
+
+    }
+}

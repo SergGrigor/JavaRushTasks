8cafcbe65221850d801f3a39d5c728a98ef39eb9
SergGrigor <vikingmasterserg@gmail.com>
2022-04-28 21:25:11 +0300

    Add solution JavaSyntax task 0424

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0424/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0424;
+
+import java.util.Scanner;
+
+/* 
+Бомба в ядре планеты
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner bombInterface = new Scanner(System.in);
+
+        //напишите тут ваш код
+        int number = 0;
+        do {
+            number = bombInterface.nextInt();
+            System.out.println(number);
+        }
+        while (number % 10 > 0);
+    }
+}

9d75f167d0b75870376aaa8b590004f9961628b2
SergGrigor <vikingmasterserg@gmail.com>
2022-05-07 02:30:44 +0300

    Add solution JavaSyntax task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String string1 = new String("Строка1");
+        String string2 = new String("Строка2");
+        String string3 = new String("Строка3");
+        String string4 = new String("Строка4");
+        String string5 = new String("Строка5");
+        String string6 = new String("Строка6");
+        String string7 = new String("Строка7");
+        String string8 = new String("Строка8");
+        String string9 = new String("Строка9");
+        String string10 = new String("Строка10");
+
+        int[] array1 = new int[1];
+        int[] array2 = new int[2];
+        int[] array3 = new int[3];
+        int[] array4 = new int[5];
+        int[] array6 = new int[5];
+
+        Scanner scanner1 = new Scanner(string1);
+        Scanner scanner2 = new Scanner(string2);
+    }
+}

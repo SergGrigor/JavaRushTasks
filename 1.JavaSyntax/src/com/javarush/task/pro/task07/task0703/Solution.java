d764029c780b15f96e0205684106b2d95e9bcff0
SergGrigor <vikingmasterserg@gmail.com>
2022-05-06 21:04:32 +0300

    Add solution JavaSyntax task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte minByte = -128;
+    short minShort = -32_768;
+    int minInt = -2_147_483_648;
+    long minLong = -9_223_372_036_854_775_808L;
+}

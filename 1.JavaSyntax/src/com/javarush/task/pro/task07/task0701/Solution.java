645f3bcba8b1632b56900d261e52749842730916
SergGrigor <vikingmasterserg@gmail.com>
2022-05-06 20:48:48 +0300

    Add solution JavaSyntax task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte numberByte = 127;
+    short numberShot = 256;
+    int numberInt = 35000;
+    long numberLong = 1_000_000_000;
+    float numberFloat = 1.26588798f;
+    double numberDouble = 123.456798123;
+    char symbolChar = '\u1234';
+    boolean valueBoolean = true;
+}

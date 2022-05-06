54409cb8065c7e84610a19b19478574c7d837fdc
SergGrigor <vikingmasterserg@gmail.com>
2022-05-06 21:21:01 +0300

    Add solution JavaSyntax task 0704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0704;
+
+/* 
+Экспонентная запись числа
+*/
+
+public class Solution {
+    double earthDiameter = 1.27420E+4;
+    double lightSpeed =2.90E+8 + 9.792458E+6;
+    double uraniumAtomicMass = 2.380289E+2;
+    double averageBeeWeight = 8.50E-2;
+    double javaDeveloperSalary = 1.0E+4;
+}

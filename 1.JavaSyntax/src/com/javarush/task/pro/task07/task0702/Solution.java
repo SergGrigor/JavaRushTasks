beb0b89a9320907b3f7c7f24b8f956e348a39309
SergGrigor <vikingmasterserg@gmail.com>
2022-05-06 21:03:00 +0300

    Add solution JavaSyntax task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte maxByte = 127;
+    short maxShort = 32_767;
+    int maxInt = 2_147_483_647;
+    long maxLong = 9_223_372_036_854_775_807L;
+}

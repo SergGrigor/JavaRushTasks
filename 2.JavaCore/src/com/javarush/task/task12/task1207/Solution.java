6ffa8d8b1a36468294ddcc95ff6b9a5dfd5559ea
SergGrigor <vikingmasterserg@gmail.com>
2022-08-04 00:20:06 +0300

    Add solution JavaCore task 1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+        print((Integer) 2);
+    }
+
+    public static void print(int number) {
+
+    }
+    public static void print(Integer number) {
+
+    }
+}

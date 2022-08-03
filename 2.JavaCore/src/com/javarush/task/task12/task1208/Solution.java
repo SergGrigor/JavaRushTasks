3b973f403a2b5bc86aac4cb292b0a091cb1b1687
SergGrigor <vikingmasterserg@gmail.com>
2022-08-04 00:23:14 +0300

    Add solution JavaCore task 1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String string) {
+
+    }
+    public static void print(Character character) {
+
+    }
+    public static void print(Boolean inf) {
+
+    }
+    public static void print(int number) {
+
+    }
+    public static void print() {
+
+    }
+}

6c26d0fdaa19ec153b28282934587d057fc88f7a
Serg <vikingmasterserg@gmail.com>
2022-08-06 20:12:10 +0300

    Add SIDE in MinesweeperGame Games minesweeper

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -4,4 +4,10 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
 
+    private static final int SIDE = 9;
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
 }

410d54c6aca8fc5ebee88755c1be2aaf13707dd0
SergGrigor <vikingmasterserg@gmail.com>
2022-08-07 18:00:23 +0300

    Add isMine in GameObject, generate mines in MinesweeperGame Games MinesweeperGame

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -6,17 +6,24 @@ public class MinesweeperGame extends Game {
 
     private static final int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private int countMinesOnField = 0;
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
         createGame();
+        System.out.println(countMinesOnField);
     }
 
     private void createGame() {
         for (int x = 0; x < gameField.length; x++) {
             for (int y = 0; y < gameField[x].length; y++) {
-                gameField[x][y] = new GameObject(y, x);
+                boolean mine = false;
+                if (getRandomNumber(10) == 5) {
+                    mine = true;
+                    countMinesOnField += 1;
+                }
+                gameField[x][y] = new GameObject(y, x, mine);
                 setCellColor(x, y, Color.ORANGE);
             }
         }

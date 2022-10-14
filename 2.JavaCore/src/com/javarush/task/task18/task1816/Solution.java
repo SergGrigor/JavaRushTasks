1513da5945217bd85099ddc469157c4afb3bf9b7
Serg <vikingmasterserg@gmail.com>
2022-10-14 15:08:12 +0300

    Add solution JavaCore task 1816

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task18.task1816;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Английские буквы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        try (FileReader reader = new FileReader(args[0])) {
+            while (reader.ready()) {
+                int read = reader.read();
+                if (read > 64 && read < 123 && Character.isAlphabetic(read)) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
+

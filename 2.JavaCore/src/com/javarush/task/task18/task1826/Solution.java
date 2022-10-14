6717cb1b9ca18a3f9eef235f784348aa016e69c4
Serg <vikingmasterserg@gmail.com>
2022-10-14 15:08:21 +0300

    Add solution JavaCore task 1826

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task18.task1826;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* 
+Шифровка
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        try (FileInputStream inputStream = new FileInputStream(args[1]);
+            FileOutputStream outputStream = new FileOutputStream(args[2])) {
+            if (args[0].equalsIgnoreCase("-e")) {
+                while (inputStream.available() > 0) {
+                    outputStream.write(inputStream.read() + 1);
+                }
+            } else if (args[0].equalsIgnoreCase("-d")) {
+                while (inputStream.available() > 0) {
+                    outputStream.write(inputStream.read() - 1);
+                }
+            }
+        }
+    }
+}

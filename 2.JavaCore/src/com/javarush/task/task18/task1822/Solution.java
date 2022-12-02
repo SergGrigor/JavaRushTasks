7f8920451fe3e9bf7c28022ff2910dbbe740c91d
SergGrigor <vikingmasterserg@gmail.com>
2022-12-02 05:59:44 +0300

    Add solution JavaCore task 1822 in process

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task18.task1822;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Поиск данных внутри файла
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id;
+        String productName;
+        double price;
+        int quantity;
+
+
+    }
+}

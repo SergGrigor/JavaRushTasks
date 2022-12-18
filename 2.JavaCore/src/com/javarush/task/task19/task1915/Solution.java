7339f3d6258f3de45d8cccb3eee8a3500fe51534
SergGrigor <vikingmasterserg@gmail.com>
2022-12-18 04:15:38 +0300

    Add solution JavaCore task 1915

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task19.task1915;
+
+import java.io.*;
+
+/* 
+Дублируем текст
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileOutputStream fileOutputStream = new FileOutputStream(reader.readLine())) {
+            PrintStream consoleStream = System.out;
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            PrintStream stream = new PrintStream(outputStream);
+            System.setOut(stream);
+            testString.printSomething();
+            fileOutputStream.write(outputStream.toByteArray());
+            System.setOut(consoleStream);
+            System.out.println(outputStream);
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+

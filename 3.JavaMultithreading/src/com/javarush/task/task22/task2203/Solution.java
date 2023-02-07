1ec1d617745a0ada4b3019ba98967d30a9a0d478
SergGrigor <vikingmasterserg@gmail.com>
2023-02-07 12:12:43 +0300

    Add solution JavaMultithreading task 2203

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string == null) {
+            throw new TooShortStringException();
+        }
+        String[] strings = string.split("\t");
+        if (strings.length <= 2) {
+            throw new TooShortStringException();
+        } else {
+            return strings[1];
+        }
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+        System.out.println(getPartOfString("JavaRush \tJavaRush - лучший сервис "));
+        System.out.println(getPartOfString(null));
+    }
+}

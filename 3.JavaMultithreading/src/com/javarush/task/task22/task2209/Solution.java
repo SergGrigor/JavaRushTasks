fdff26f64c69107dcb1edca77a1bab014b80d17f
SergGrigor <vikingmasterserg@gmail.com>
2023-02-12 03:05:50 +0300

    Add solution JavaMultithreading task 2209

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -0,0 +1,95 @@
+package com.javarush.task.task22.task2209;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+Составить цепочку слов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        List<String> list = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))) {
+            while (reader.ready()) {
+                list.add(reader.readLine());
+        }
+            List<String> resultList = new ArrayList<>();
+            for (String line : list) {
+                StringTokenizer tokenizer = new StringTokenizer(line);
+                while (tokenizer.hasMoreTokens()) {
+                    resultList.add(tokenizer.nextToken());
+                }
+            }
+
+            StringBuilder result = getLine(getWords(resultList));
+            System.out.println(result.toString());
+        }
+    }
+
+    private static boolean isTheSameChars(String firstWord, String secondWord) {
+        if (firstWord.endsWith(" ")) {
+            firstWord = firstWord.substring(0, firstWord.length() - 1);
+        }
+        return firstWord.isEmpty() || (secondWord != null &&
+                Character.toUpperCase(firstWord.charAt(firstWord.length() - 1)) == Character.toUpperCase(secondWord.charAt(0)));
+    }
+
+    private static <T> T getLastElement(List<? extends T> list) {
+        return list.get(list.size() - 1);
+    }
+
+
+    public static StringBuilder getLine(String... words) {
+        StringBuilder builder = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            list.add(i);
+            if (findSolutions(list, words)) {
+                for (Integer integer : list) {
+                    builder.append(words[integer]);
+                    builder.append(" ");
+                }
+                return builder;
+            }
+            list.remove(Integer.valueOf(i));
+        }
+
+        return builder;
+    }
+
+    private static String[] getWords(List<String> list) {
+        String[] array = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+
+    private static boolean findSolutions(List<Integer> list, String... words) {
+        if (list.size() == words.length) {
+            return true;
+        }
+        for (int i = 0; i < words.length; i++) {
+            if (isValid(list, words[i], words)) {
+                list.add(i);
+                if (findSolutions(list, words)) {
+                    return true;
+                }
+                list.remove(Integer.valueOf(i));
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValid(List<Integer> list, String word, String... words) {
+        for (Integer integer : list) {
+            if (words[integer].equals(word)) {
+                return false;
+            }
+        }
+        return isTheSameChars(words[getLastElement(list)], word);
+    }
+}

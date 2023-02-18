633a40cb4612e6b046586cb0162edce129ae9281
SergGrigor <vikingmasterserg@gmail.com>
2023-02-19 01:17:05 +0300

    Add solution JavaMultithreading task 2309

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/Location.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task23.task2309.vo;
+
+import com.javarush.task.task23.task2309.AbstractDbSelectExecutor;
+
+import java.util.List;
+
+public class Location extends NamedItem {
+
+
+}
